645afb661cc7b586ea8e4f5b6b56751f04441824
HaeunKim <rlagkdms26@gmail.com>
2023-07-12 12:16:24 +0900

    Update OAuth2MemberSuccessHandler.java

--- src/main/java/wanderhub/server/auth/handler/OAuth2MemberSuccessHandler.java
+++ src/main/java/wanderhub/server/auth/handler/OAuth2MemberSuccessHandler.java
@@ -127,7 +127,7 @@ public class OAuth2MemberSuccessHandler extends SimpleUrlAuthenticationSuccessHa
                 .scheme("http")
                 .host("localhost")
                 .port(3000)    // 확인하기.
-                .path("/ouath/redirect/receive-token")
+                .path("/oauth/redirect")
                 .queryParams(queryParams)
                 .build()
                 .toUri();
